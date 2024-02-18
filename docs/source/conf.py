c833089f71bd5db9fea4f079421a1aca90eba8c8
bryce-wedig <84239923+bryce-wedig@users.noreply.github.com>
2024-02-18 12:28:11 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # https://stackoverflow.com/questions/67485175/docstrings-are-not-included-in-read-the-docs-sphinx-build/67486947#67486947
-autodoc_mock_imports = ['numpy', 'pandas', 'PIL']
+autodoc_mock_imports = ['numpy', 'pandas', 'PIL', 'astropy', 'omegaconf']
 
 # -- Options for HTML output
 
